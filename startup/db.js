4c3fff1dbef1a2a88ed4cdecbdb528b23808a667
Jabbar Ahmad <jabbarahmad540@gmail.com>
2024-09-23 15:08:59 +0500

    don

--- startup/db.js
+++ startup/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const logger = require('./logger'); // Adjust the path as needed
 
 module.exports = function () {
-  const db = 'mongodb+srv://utecho683:f2uETWZL1gKn21D7@cluster0.dsviepk.mongodb.net/gym';
+  const db = 'mongodb+srv://utecho683:f2uETWZL1gKn21D7@cluster0.dsviepk.mongodb.net/eventshub';
   mongoose.connect(db)
     .then(() => logger.info(`Connected to ${db}...`));
 }
