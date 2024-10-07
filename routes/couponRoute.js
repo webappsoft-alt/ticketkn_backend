5ad27722ca334f269ebb1a0aa4c1739f9b3cb0b9
Jabbar Ahmad <jabbarahmad540@gmail.com>
2024-10-07 14:51:18 +0500

    complete

--- routes/couponRoute.js
+++ routes/couponRoute.js
@@ -5,7 +5,7 @@ const auth = require('../middleware/auth');
 
 router.post('/create', auth,categoriesController.create);
 router.put('/edit/:id',auth,  categoriesController.editCategories);
-router.get('/me/:id',auth, categoriesController.getMyCoupons);
+router.get('/me/:id?',auth, categoriesController.getMyCoupons);
 router.delete('/:id',auth, categoriesController.deleteCoupons);
 
 module.exports = router;
