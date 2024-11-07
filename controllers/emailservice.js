753574946374574b2a10c73522c3f0aa113d1b4a
Jabbar Ahmad <jabbarahmad540@gmail.com>
2024-11-07 18:01:29 +0500

    payment

--- controllers/emailservice.js
+++ controllers/emailservice.js
@@ -1,19 +1,23 @@
+require('dotenv').config();
+
 const nodemailer = require('nodemailer');
 const logger = require('../startup/logger'); // Adjust the path as needed
 
 exports.sendEmail = async (email, code) => {
      // Create a Nodemailer transporter object
      const transporter = nodemailer.createTransport({
-          service: 'Gmail',
+          host: 'smtp.office365.com', // SMTP server address for Outlook
+          port: 587, // SMTP port
+          secure: false, // Set to true for port 465, false for others
           auth: {
-               user: 'danishgoheer17@gmail.com',
-               pass: 'zzmftuogtusnnriu',
-          },
+            user: 'Support@ticketkn.com', // Your Outlook email
+            pass: process.env.EMAIL_PASSWORD // Your Outlook email password or app password
+          }
      });
 
      // Email data
      const mailOptions = {
-          from: 'danishgoheer17@gmail.com',
+          from: 'Support@ticketkn.com',
           to: email, // Replace with the recipient's email address
           subject: 'TicketKN app Verification',
           text: 'Your TicketKN app verification code is ' + code,
