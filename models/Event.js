4d6d3c3c30138953aecebe9990de7cd1db043bc7
Arslan Mubarak <arslan.saqib121@gmail.com>
2026-05-23 19:23:03 +0500

    feat: create Event mongoose model with ticket and location schemas

--- models/Event.js
+++ models/Event.js
@@ -48,63 +48,68 @@ const ticketObj = {
   },
 };
 
-const postSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
-  images: [String],
-  name: String,
-  event_type: String,
-  start_Date: Date,
-  start_Time: Date,
-  address: String,
-  country: String,
-  city: String,
-  state: String,
-  description: String,
-  join_people: {
-    type: Number,
-    default: 0,
-  },
-  total_tickets_sale: {
-    type: Number,
-    default: 0,
-  },
-  tickets_sale: [ticketObj],
-  ticket_plans: [ticketPlanObj],
-  refund_policy: String,
-  coupon: { type: mongoose.Schema.Types.ObjectId, ref: "Coupon" },
-  category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Like" }], // Reference to likes
-  purchase_by: [{ type: mongoose.Schema.Types.ObjectId, ref: "Purchase" }], // Reference to likes
-  createdAt: { type: Date, default: Date.now, index: true }, // Timestamp for post creation
-  location: {
-    type: { type: String, enum: ["Point"], default: "Point" },
-    coordinates: { type: [Number], required: true },
-  },
-  paymentDone: {
-    type: Boolean,
-    default: false,
-  },
-  payment: [
-    {
-      amount: Number,
-      date: Date,
+const postSchema = new mongoose.Schema(
+  {
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    images: [String],
+    name: String,
+    event_type: String,
+    start_Date: Date,
+    start_Time: Date,
+    address: String,
+    country: String,
+    city: String,
+    state: String,
+    description: String,
+    join_people: {
+      type: Number,
+      default: 0,
     },
-  ],
-  popular: {
-    type: Boolean,
-    default: false,
+    total_tickets_sale: {
+      type: Number,
+      default: 0,
+    },
+    tickets_sale: [ticketObj],
+    ticket_plans: [ticketPlanObj],
+    refund_policy: String,
+    coupon: { type: mongoose.Schema.Types.ObjectId, ref: "Coupon" },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: "Category" },
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "Like" }], // Reference to likes
+    purchase_by: [{ type: mongoose.Schema.Types.ObjectId, ref: "Purchase" }], // Reference to likes
+    createdAt: { type: Date, default: Date.now, index: true }, // Timestamp for post creation
+    location: {
+      type: { type: String, enum: ["Point"], default: "Point" },
+      coordinates: { type: [Number], required: true },
+    },
+    paymentDone: {
+      type: Boolean,
+      default: false,
+    },
+    payment: [
+      {
+        amount: Number,
+        date: Date,
+      },
+    ],
+    popular: {
+      type: Boolean,
+      default: false,
+    },
+    status: {
+      type: String,
+      default: "active",
+      enum: ["active", "deleted"],
+    },
+    type: {
+      type: String,
+      default: "free",
+      enum: ["free", "paid"],
+    },
   },
-  status: {
-    type: String,
-    default: "active",
-    enum: ["active", "deleted"],
+  {
+    timestamps: true,
   },
-  type: {
-    type: String,
-    default: "free",
-    enum: ["free", "paid"],
-  },
-});
+);
 
 postSchema.index({ location: "2dsphere" });
 
